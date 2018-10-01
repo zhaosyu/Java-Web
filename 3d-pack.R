4232b2061eace3c746d9fecc321bf4495d761de9
zhaosyu <31990038+zhaosyu@users.noreply.github.com>
2018-10-01 14:12:08 +0800

    Update 3d-pack.R

--- 3d-pack.R
+++ 3d-pack.R
@@ -216,7 +216,7 @@ all<-function(x0,y0,z0){
 pathTemp<-unlist(strsplit(pathAllT_Data, "[.]"))
 mypal<-rev(rainbow(500,start = 0,end=1/3))
 titleRgl<-unlist(strsplit(pathTemp[1], "[\\,/]"))
-titleRgl<-titleRgl[length(titleRgl)]
+titleRgl<-paste0(titleRgl[length(titleRgl)],"后视图")
 #先打开一个角度，别关这个窗口
 #打开窗口，调整视角画好图例
 library(fields)
@@ -245,12 +245,25 @@ for (mmm in seq(1,nrow(ALLT),by= 20)){
   df_AT$T<- all(df_AT$x,df_AT$y,df_AT$z)
   #将颜色数据保存追加写入csv,数据横着放，一行大概10s，一个文件50*10s~~10min
   write.table(t(df_AT$T),paste0(pathTemp[1],"_后视图_Temp.csv"),sep=",",append=TRUE,row.names=FALSE,col.names=FALSE) 
-  picture <- cut(c(df_AT$T,10,60),breaks=500) #10~60,0.1度一区间
-  #cols <- rainbow(50,start = 0,end=1/3)[as.numeric(picture[1:(length(picture)-2)])]
+  #picture <- cut(c(df_AT$T,10,60),breaks=500) #10~60,0.1度一区间
+  ##cols <- rainbow(50,start = 0,end=1/3)[as.numeric(picture[1:(length(picture)-2)])]
+  #cols <- mypal[as.numeric(picture[1:(length(picture)-2)])]
+  ##open3d()
+  #plot3d(df_AT$x, df_AT$y, df_AT$z,aspect = c(X, Y, Z),xlab = "",ylab = "",zlab ="",col=cols,type="p",size=2,axes = FALSE)
+  #wire3d(translate3d(pm,X/2,Y/2,Z/2),col = "lightgrey",alpha=0.3)
+  #snapshot3d(paste0("C:/Users/Administrator/Desktop/battery_model_4p12s/images/温度20度后_ ",mmm,".png"))
+  #rgl.close()
+}
+
+library(data.table)
+dataT<-fread(paste0(pathTemp[1],"_Temp.csv"))
+dataT<-t(dataT)
+
+for(i in seq(ncol(dataT))){
+  T<-dataT[,i]
+  picture <- cut(c(T,10,60),breaks=500) #10~60,0.1度一区间
   cols <- mypal[as.numeric(picture[1:(length(picture)-2)])]
-  #open3d()
   plot3d(df_AT$x, df_AT$y, df_AT$z,aspect = c(X, Y, Z),xlab = "",ylab = "",zlab ="",col=cols,type="p",size=2,axes = FALSE)
   wire3d(translate3d(pm,X/2,Y/2,Z/2),col = "lightgrey",alpha=0.3)
   snapshot3d(paste0("C:/Users/Administrator/Desktop/battery_model_4p12s/images/温度20度后_ ",mmm,".png"))
-  #rgl.close()
 }
